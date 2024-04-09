339559d51bb5c7b3e60847ee981df5255935647d
dltkd1395 <dltkd13956@naver.com>
2024-04-09 23:50:07 +0900

    feat: Node 클래스 및 하위 클래스 CircleNode, RectangleNode 추가

---
+++ library/src/main/java/com/mindsync/library/data/Node.kt
@@ -0,0 +1,51 @@
+package com.mindsync.mindmap.data
+
+import com.mindsync.mindmap.util.Dp
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
+
+sealed class Node(
+    open val id: String,
+    open val parentId: String?,
+    open val path: NodePath,
+    open val description: String,
+    open val children: List<String>,
+    open var alpha: Float,
+    open var isAnimating: Boolean,
+    open var isDrawingLine: Boolean,
+    open var strokeWidth: Float,
+) : java.io.Serializable {
+    abstract fun adjustPosition(horizontalSpacing: Dp, totalHeight: Dp): Node
+}
+
+data class CircleNode(
+    override val id: String,
+    override val parentId: String?,
+    override val path: CirclePath = CirclePath(Dp(0f), Dp(0f), Dp(0f)),
+    override val description: String,
+    override val children: List<String>,
+    override var alpha: Float = 0f,
+    override var isAnimating: Boolean = false,
+    override var isDrawingLine: Boolean = false,
+    override var strokeWidth: Float = 1f,
+) : Node(id, parentId, path, description, children, alpha, isAnimating, isDrawingLine, strokeWidth) {
+    override fun adjustPosition(horizontalSpacing: Dp, totalHeight: Dp): Node {
+        return this.copy(path = path.adjustPath(horizontalSpacing, totalHeight))
+    }
+}
+
+data class RectangleNode(
+    override val id: String,
+    override val parentId: String,
+    override val path: RectanglePath = RectanglePath(Dp(0f), Dp(0f), Dp(0f), Dp(0f)),
+    override val description: String,
+    override val children: List<String>,
+    override var alpha: Float = 0f,
+    override var isAnimating: Boolean = false,
+    override var isDrawingLine: Boolean = false,
+    override var strokeWidth: Float = 1f,
+) : Node(id, parentId, path, description, children, alpha, isAnimating, isDrawingLine, strokeWidth) {
+    override fun adjustPosition(horizontalSpacing: Dp, totalHeight: Dp): Node {
+        return this.copy(path = path.adjustPath(horizontalSpacing, totalHeight))
+    }
+}
