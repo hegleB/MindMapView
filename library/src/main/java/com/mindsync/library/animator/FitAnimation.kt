fd9091582508363e80a27c9e0adacdc300825ab4
dltkd1395 <dltkd13956@naver.com>
2024-04-10 00:00:37 +0900

    feat: FitAnimation 추가

---
+++ library/src/main/java/com/mindsync/library/animator/FitAnimation.kt
@@ -0,0 +1,25 @@
+package com.mindsync.mindmap.animator
+
+import android.animation.PropertyValuesHolder
+import com.mindsync.mindmap.animator.AnimationUtils.START_PROCESS
+
+class FitAnimation(
+    private val startScaleFactor: Float,
+    private val endScaleFactor: Float,
+    private val endX: Float,
+    private val endY: Float,
+    private val update: (Float, Float, Float) -> Unit
+) : AnimationStrategy {
+    override fun animate() {
+        val scaleXHolder =
+            PropertyValuesHolder.ofFloat("scaleFactor", startScaleFactor, endScaleFactor)
+        val translateXHolder = PropertyValuesHolder.ofFloat("currentPointX", endX, START_PROCESS)
+        val translateYHolder = PropertyValuesHolder.ofFloat("currentPointY", endY, START_PROCESS)
+
+        AnimationUtils.createPropertyValuesHolderAnimator(
+            scaleXHolder, translateXHolder, translateYHolder
+        ) { scale, currentX, currentY ->
+            update(scale, currentX, currentY)
+        }.start()
+    }
+}
