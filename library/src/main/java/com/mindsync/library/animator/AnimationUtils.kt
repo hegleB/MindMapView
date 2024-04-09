691c96d9f70066bdc1377082e453d5860c2371a5
dltkd1395 <dltkd13956@naver.com>
2024-04-10 00:00:17 +0900

    feat: AnimationUtils 추가

---
+++ library/src/main/java/com/mindsync/library/animator/AnimationUtils.kt
@@ -0,0 +1,45 @@
+package com.mindsync.mindmap.animator
+
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.animation.ValueAnimator
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.Interpolator
+
+object AnimationUtils {
+    const val ANIMATION_DURATION = 300L
+    const val START_PROCESS = 0f
+    const val END_PROCESS = 1f
+
+    fun createObjectAnimator(
+        start: Float,
+        end: Float,
+        duration: Long = ANIMATION_DURATION,
+        interpolator: Interpolator = AccelerateDecelerateInterpolator(),
+        onUpdate: (Float) -> Unit
+    ) = ObjectAnimator.ofFloat(start, end).apply {
+        this.duration = duration
+        this.interpolator = interpolator
+        addUpdateListener { animation ->
+            val animatedValue = animation.animatedValue as Float
+            onUpdate(animatedValue)
+        }
+    }
+
+    fun createPropertyValuesHolderAnimator(
+        vararg holderValue: PropertyValuesHolder,
+        duration: Long = ANIMATION_DURATION,
+        interpolator: Interpolator = AccelerateDecelerateInterpolator(),
+        onUpdate: (scale: Float, currentX: Float, currentY: Float) -> Unit
+    ) = ValueAnimator.ofPropertyValuesHolder(*holderValue)
+        .apply {
+            this.duration = duration
+            this.interpolator = interpolator
+            addUpdateListener { animation ->
+                val scale = animation.getAnimatedValue("scaleFactor") as Float
+                val x = animation.getAnimatedValue("currentPointX") as Float
+                val y = animation.getAnimatedValue("currentPointY") as Float
+                onUpdate(scale, x, y)
+            }
+        }
+}
