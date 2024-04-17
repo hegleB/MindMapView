e39eecb497d37480ffb47d02d300f306df03236a
dltkd1395 <dltkd13956@naver.com>
2024-04-17 23:23:53 +0900

    feat: EditDescriptionViewModel 추가

---
+++ app/src/main/java/com/mindsync/mindmapview/EditDescriptionViewModel.kt
@@ -0,0 +1,19 @@
+package com.mindsync.mindmapview
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class EditDescriptionViewModel : ViewModel() {
+    private val _description = MutableStateFlow("")
+    val description: StateFlow<String> = _description
+    var submitListener: ((String) -> (Unit))? = null
+
+    fun setDescription(text: String) {
+        _description.value = text
+    }
+
+    fun onDescriptionChanged(inputDescription: CharSequence) {
+        _description.value = inputDescription.toString()
+    }
+}
