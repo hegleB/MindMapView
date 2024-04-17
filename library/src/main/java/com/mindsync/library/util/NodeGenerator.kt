eba82953e142cfbc39a9fb8a782a2cc1d937b3f1
dltkd1395 <dltkd13956@naver.com>
2024-04-17 23:15:28 +0900

    refactor: makeNode 함수 제너릭화 적용

--- library/src/main/java/com/mindsync/library/util/NodeGenerator.kt
+++ library/src/main/java/com/mindsync/library/util/NodeGenerator.kt
@@ -1,24 +1,37 @@
 package com.mindsync.library.util
 
-import com.mindsync.library.data.RectangleNode
+import com.mindsync.library.data.CircleNodeData
+import com.mindsync.library.data.CirclePath
+import com.mindsync.library.data.NodeData
+import com.mindsync.library.data.RectangleNodeData
 import com.mindsync.library.data.RectanglePath
+import kotlin.reflect.KClass
 
 
 object NodeGenerator {
-    fun makeNode(
+    fun <N : NodeData<*>> makeNode(
+        nodeClass: KClass<N>,
         description: String,
-        parentId: String,
-    ) = RectangleNode(
-        id = IdGenerator.makeRandomNodeId(),
-        parentId = parentId,
-        path =
-            RectanglePath(
-                Dp(0f),
-                Dp(0f),
-                Dp(0f),
-                Dp(0f),
-            ),
-        description = description,
-        listOf(),
-    )
+        parentId: String
+    ): N {
+        val id = IdGenerator.makeRandomNodeId()
+
+        return when (nodeClass) {
+            CircleNodeData::class -> CircleNodeData(
+                id = id,
+                parentId = parentId,
+                path = CirclePath(Dp(0f), Dp(0f), Dp(50f)),  // 예시로 CirclePath 설정
+                description = description,
+                children = listOf()
+            ) as N
+            RectangleNodeData::class -> RectangleNodeData(
+                id = id,
+                parentId = parentId,
+                path = RectanglePath(Dp(0f), Dp(0f), Dp(100f), Dp(50f)),  // 예시로 RectanglePath 설정
+                description = description,
+                children = listOf()
+            ) as N
+            else -> throw IllegalArgumentException("Unsupported Node type")
+        }
+    }
 }
