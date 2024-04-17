e536066b026d6576e4706aa435b3d51845436972
dltkd1395 <dltkd13956@naver.com>
2024-04-17 23:21:31 +0900

    feat: Sample Model 추가

---
+++ app/src/main/java/com/mindsync/mindmapview/model/Node.kt
@@ -0,0 +1,47 @@
+package com.mindsync.mindmapview.model
+
+import com.mindsync.mindmapview.Dp
+import kotlinx.serialization.Serializable
+
+sealed class Node(
+    open val id: String,
+    open val parentId: String?,
+    open val path: NodePath,
+    open val description: String,
+    open val children: List<String>,
+) {
+    abstract fun adjustPosition(
+        horizontalSpacing: Dp,
+        totalHeight: Dp,
+    ): Node
+}
+
+data class CircleNode(
+    override val id: String,
+    override val parentId: String?,
+    override val path: CirclePath = CirclePath(Dp(0f), Dp(0f), Dp(0f)),
+    override val description: String,
+    override val children: List<String>,
+) : Node(id, parentId, path, description, children) {
+    override fun adjustPosition(
+        horizontalSpacing: Dp,
+        totalHeight: Dp,
+    ): Node {
+        return this.copy(path = path.adjustPath(horizontalSpacing, totalHeight))
+    }
+}
+
+data class RectangleNode(
+    override val id: String,
+    override val parentId: String,
+    override val path: RectanglePath = RectanglePath(Dp(0f), Dp(0f), Dp(0f), Dp(0f)),
+    override val description: String,
+    override val children: List<String>,
+) : Node(id, parentId, path, description, children) {
+    override fun adjustPosition(
+        horizontalSpacing: Dp,
+        totalHeight: Dp,
+    ): Node {
+        return this.copy(path = path.adjustPath(horizontalSpacing, totalHeight))
+    }
+}
---
+++ app/src/main/java/com/mindsync/mindmapview/model/NodePath.kt
@@ -0,0 +1,53 @@
+package com.mindsync.mindmapview.model
+
+import com.mindsync.mindmapview.Dp
+import kotlinx.serialization.Serializable
+
+sealed class NodePath(open val centerX: Dp, open val centerY: Dp) {
+    abstract fun adjustPath(
+        horizontalSpacing: Dp,
+        totalHeight: Dp,
+    ): NodePath
+
+    protected fun calculateNewCenterY(
+        horizontalSpacing: Dp,
+        totalHeight: Dp,
+    ): Dp {
+        return centerY + totalHeight / 2 + horizontalSpacing
+    }
+}
+
+data class RectanglePath(
+    override val centerX: Dp,
+    override val centerY: Dp,
+    val width: Dp,
+    val height: Dp,
+) : NodePath(centerX, centerY) {
+    fun leftX() = centerX - (width / (Dp(2f)))
+
+    fun topY() = centerY - (height / (Dp(2f)))
+
+    fun rightX() = centerX + (width / (Dp(2f)))
+
+    fun bottomY() = centerY + (height / (Dp(2f)))
+
+    override fun adjustPath(
+        horizontalSpacing: Dp,
+        totalHeight: Dp,
+    ): RectanglePath {
+        return this.copy(centerY = calculateNewCenterY(horizontalSpacing, totalHeight))
+    }
+}
+
+data class CirclePath(
+    override val centerX: Dp,
+    override val centerY: Dp,
+    val radius: Dp,
+) : NodePath(centerX, centerY) {
+    override fun adjustPath(
+        horizontalSpacing: Dp,
+        totalHeight: Dp,
+    ): CirclePath {
+        return this.copy(centerY = calculateNewCenterY(horizontalSpacing, totalHeight))
+    }
+}
